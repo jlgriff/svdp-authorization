c061759e7a7d2cdea736f1c0cf4bf295efdc2d36
jlgriff <jlgriffith@protonmail.com>
2022-12-28 15:08:18 -0600

    Fixed missing access level labels.

--- dist/src/constants/role.js
+++ dist/src/constants/role.js
@@ -12,7 +12,7 @@ export const ACCESS_LEVEL_MAPPINGS = [
     { access: AccessLevel.CONTRIBUTOR, tokenCode: new Uint8Array([20]), label: AccessLevel.CONTRIBUTOR.toString() },
     { access: AccessLevel.APPROVER, tokenCode: new Uint8Array([30]), label: AccessLevel.APPROVER.toString() },
     { access: AccessLevel.ADMINISTRATOR, tokenCode: new Uint8Array([40]), label: AccessLevel.ADMINISTRATOR.toString() },
-    { access: AccessLevel.SYSTEM, tokenCode: new Uint8Array([200]), label: AccessLevel.ADMINISTRATOR.toString() },
-    { access: AccessLevel.SYSTEM_ADMINISTRATOR, tokenCode: new Uint8Array([210]), label: AccessLevel.ADMINISTRATOR.toString() },
+    { access: AccessLevel.SYSTEM, tokenCode: new Uint8Array([200]), label: AccessLevel.SYSTEM.toString() },
+    { access: AccessLevel.SYSTEM_ADMINISTRATOR, tokenCode: new Uint8Array([210]), label: AccessLevel.SYSTEM_ADMINISTRATOR.toString() },
 ];
 //# sourceMappingURL=role.js.map
